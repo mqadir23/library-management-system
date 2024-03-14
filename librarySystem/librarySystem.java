3a1c78efd4beb3046d04f3d09c064e4a7edb24a5
mqadir23 <144931474+mqadir23@users.noreply.github.com>
2024-03-14 09:35:37 +0500

    Create librarySystem.java

---
+++ librarySystem/librarySystem.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package JFrame;
+
+
+/**
+ *
+ * @author Lenovo
+ */
+public class LibrarySystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //calling the iniitial point of whole program
+       SignUpPage page=new SignUpPage();
+       page.setVisible(true);
+        
+    }
+    
+}
